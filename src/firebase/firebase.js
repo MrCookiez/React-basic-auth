671a73490d4450e57f0e8098aea8308a28e2c6e5
Theodoros Vragkos <bigt3ddybear@yahoo.gr>
2018-08-26 16:31:13 +0300

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyBrul2KvktqXh_Gca7oSQ102PuEw-aTgv8",
-  authDomain: "order-app-4563d.firebaseapp.com",
-  databaseURL: "https://order-app-4563d.firebaseio.com",
-  projectId: "order-app-4563d",
-  storageBucket: "order-app-4563d.appspot.com",
-  messagingSenderId: "444367037690"
+  apiKey: "XXXXXXXXXX",
+  authDomain: "XXXXXXXXXXXXXXX",
+  databaseURL: "XXXXXXXXXXXXXX",
+  projectId: "XXXXX",
+  storageBucket: "XXXXX",
+  messagingSenderId: "XXXXX"
 };
 
 firebase.initializeApp(config);
